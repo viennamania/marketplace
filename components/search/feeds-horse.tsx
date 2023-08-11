b06e7cc741ee1a153541dfa10eef4c16668d9cbc
seunghyonpark <creath.park@gmail.com>
2023-08-11 19:23:45 +0900

    hello world

--- components/search/feeds-horse.tsx
+++ components/search/feeds-horse.tsx
@@ -258,13 +258,13 @@ export default function Feeds({ className }: { className?: string }) {
                       }
                     >
 
+
                       <Image
                         src={nft?.media[0]?.gateway ? nft?.media[0]?.gateway : '/default-nft.png' }
                         alt={nft?.title}
                         height={500}
                         width={500}
                         loading='lazy'
-                        
                       />
                       <div className='w-full m-2'>
                         <p className='text-md font-bold'>{nft?.title}</p>

4072f3af9e019ae11a02a79be83d360b9b9b777b
seunghyonpark <creath.park@gmail.com>
2023-08-11 13:16:50 +0900

    gogo sing

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,17 +26,132 @@ module.exports = {
     },
 
     extend: {
+      colors: {
+        brand: 'rgb(var(--color-brand) / <alpha-value>)',
+        body: '#fcfcfc',
+        dark: '#0D1321',
+        'light-dark': '#171e2e',
+        'sidebar-body': '#F8FAFC',
+      },
+
+      spacing: {
+        13: '3.375rem',
+      },
+      margin: {
+        '1/2': '50%',
+      },
+      padding: {
+        full: '100%',
+      },
+      width: {
+        'calc-320': 'calc(100% - 320px)',
+        'calc-358': 'calc(100% - 358px)',
+      },
+      fontFamily: {
+        //body: ['Fira Code', 'monospace'],
+        body: ['Pixellari', 'sans-serif'],
+      },
+      fontSize: {
+        '13px': ['13px', '18px'],
+      },
+      borderWidth: {
+        3: '3px',
+      },
+      boxShadow: {
+        main: '0px 6px 18px rgba(0, 0, 0, 0.04)',
+        light: '0px 4px 4px rgba(0, 0, 0, 0.08)',
+        large: '0px 8px 16px rgba(17, 24, 39, 0.1)',
+        card: '0px 2px 6px rgba(0, 0, 0, 0.06)',
+        transaction: '0px 8px 16px rgba(17, 24, 39, 0.06)',
+        expand: '0px 0px 50px rgba(17, 24, 39, 0.2)',
+        button:
+          '0px 2px 4px rgba(0, 0, 0, 0.06), 0px 4px 6px rgba(0, 0, 0, 0.1)',
+      },
+      dropShadow: {
+        main: '0px 4px 8px rgba(0, 0, 0, 0.08)',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      animation: {
+        blink: 'blink 1.4s infinite both;',
+        'move-up': 'moveUp 500ms infinite alternate',
+        'scale-up': 'scaleUp 500ms infinite alternate',
+        'drip-expand': 'expand 500ms ease-in forwards',
+        'drip-expand-large': 'expand-large 600ms ease-in forwards',
+        'move-up-small': 'moveUpSmall 500ms infinite alternate',
+      },
+      keyframes: {
+        blink: {
+          '0%': { opacity: 0.2 },
+          '20%': { opacity: 1 },
+          '100%': { opacity: 0.2 },
+        },
+        expand: {
+          '0%': {
+            opacity: 0,
+            transform: 'scale(1)',
+          },
+          '30%': {
+            opacity: 1,
+          },
+          '80%': {
+            opacity: 0.5,
+          },
+          '100%': {
+            transform: 'scale(30)',
+            opacity: 0,
+          },
+        },
+        'expand-large': {
+          '0%': {
+            opacity: 0,
+            transform: 'scale(1)',
+          },
+          '30%': {
+            opacity: 1,
+          },
+          '80%': {
+            opacity: 0.5,
+          },
+          '100%': {
+            transform: 'scale(96)',
+            opacity: 0,
+          },
+        },
+        moveUp: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(-20px)' },
+        },
+        moveUpSmall: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(-10px)' },
+        },
+        scaleUp: {
+          '0%': { transform: 'scale(0)' },
+          '100%': { transform: 'scale(1)' },
+        },
+      },
+
+
+      /*
       animation: {
         flip:'flip 1s cubic-bezier(0, 0, 0.2, 1) infinite'
       },
+
+
+
       keyframes:{
         flip: {
           'from': { transform:  'rotateX(0deg)', transformOrigin: '50% bottom ', },
           'to':{transform:  'rotateX(180deg)', transformOrigin: '50% bottom ',}
         }
       }
+      */
+
     },
   },
-  plugins: [],
+  ////plugins: [],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 }
 
